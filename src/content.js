ac9150ffc915da20d299e97ccf61466fad2d8b6e
p9yp9y <p9yp9y@gmail.com>
2020-08-05 06:56:48 +0200

    bla

--- src/content.js
+++ src/content.js
@@ -1,10 +1,29 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    return {redirectUrl: chrome.extension.getURL("player.html") + "?" + decodeURIComponent(info.url)};
+
+
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details){
+	if (!details.url.endsWith("nofilter")) {
+		for(a in details.responseHeaders) {
+			var v = evalHeader(details.responseHeaders[a]);
+			if (v) {
+				return {redirectUrl: chrome.extension.getURL("player.html") + "?" + decodeURIComponent(details.url)};
+			}
+		}
+	}
   },
   {
     urls: [
-      "*://*/*.m3u"
+      "*://*/*"
     ]
   },
-  ["blocking"]);
+  ["blocking", "responseHeaders"]);
+
+function evalHeader(h) {
+	if (h.name.toUpperCase()=="CONTENT-TYPE") {
+		var v = h.value.toUpperCase();
+		if (v.startsWith("AUDIO")) {
+			return true;
+		}
+	}
+	return false;
+}
