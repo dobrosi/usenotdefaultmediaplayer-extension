6ef8eb47a088a12888d0249dc96007f099cd8c58
Dobrosi Andras <dobrosi@gmail.com>
2022-01-27 19:08:13 +0100

    new player

--- src/content.js
+++ src/content.js
@@ -1,29 +1,11 @@
-
-
-chrome.webRequest.onHeadersReceived.addListener(
-  function(details){
-	if (!details.url.endsWith("nofilter")) {
-		for(a in details.responseHeaders) {
-			var v = evalHeader(details.responseHeaders[a]);
-			if (v) {
-				return {redirectUrl: chrome.extension.getURL("player.html") + "?" + decodeURIComponent(details.url)};
-			}
-		}
+if (document.body.childElementCount == 1) {
+	firstTag = document.body.firstElementChild;
+	if (firstTag.tagName.toLowerCase() == 'video') {
+		sourceTag = document.body.firstElementChild.firstElementChild;
+		document.head.innerHTML = "<link href=\"https://vjs.zencdn.net/7.17.0/video-js.css\" rel=\"stylesheet\" />";
+		document.body.innerHTML = "<video id=\"my-video\" class=\"video-js\"" +
+			"controls autoplay preload=\"auto\" width=\"100%\" height=\"100%\" data-setup=\"{}\">" + 
+			"<source src=\"" + sourceTag.src +  "\" type=\"" + sourceTag.type + "\" />" +
+			"</video><script src=\"https://vjs.zencdn.net/7.17.0/video.min.js\"></script>";
 	}
-  },
-  {
-    urls: [
-      "*://*/*"
-    ]
-  },
-  ["blocking", "responseHeaders"]);
-
-function evalHeader(h) {
-	if (h.name.toUpperCase()=="CONTENT-TYPE") {
-		var v = h.value.toUpperCase();
-		if (v.startsWith("AUDIO")) {
-			return true;
-		}
-	}
-	return false;
 }
